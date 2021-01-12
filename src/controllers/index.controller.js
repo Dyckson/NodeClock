cd147325c7feae5f01d4d40fda79d1ea064f7131
dyckson <dyckson_vyctor@hotmail.com>
2021-01-12 10:18:36 -0300

    metodos get criando caso não exista

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -19,11 +19,27 @@ const getClockByH = async (req, res) => {
     const hour = parseInt(req.params.hour);
     const minute = 0;
     const response = await pool.query('SELECT angle FROM clock WHERE hour = $1 AND minute = $2', [hour, minute]);
-    res.json(response.rows[0]);
 
     if(typeof response.rows[0] == "undefined"){
-        
-    };
+        let date = new Date();
+        let angle = ((11 * minute - 60 * hour)/2);
+        angle = Math.round(Math.abs(angle));
+        if (angle > 180){
+            angle = 360 - angle
+        };
+
+        if(hour >= 0 & hour <= 11){
+            const response = await pool.query('INSERT INTO public.clock (hour, minute, angle, date) VALUES ($1, $2, $3, $4)', [hour, minute, angle, date]);
+            res.json({
+                message: 'Clock cadastrado com sucesso',
+                clock: {hour, minute, angle, date}
+            })
+        } else{
+            res.json('O valor de horas deve está entre 0 e 11')
+        };
+        }else{
+            res.json(response.rows[0]);
+        }
 };
 
 //Retorna dados buscados por horas e minutos
@@ -31,79 +47,30 @@ const getClockByHM = async (req, res) => {
     const hour = parseInt(req.params.hour);
     const minute = parseInt(req.params.minute);
     const response = await pool.query('SELECT angle FROM clock WHERE hour = $1 AND minute = $2', [hour, minute]);
-    res.json(response.rows[0]);
-};
-
-//Adiciona dados via URL tendo hora como único parâmetro
-const postClockHour = async (req, res) => {
-    const hour = parseInt(req.params.hour)
-    const minute = 0
-    // retorna a data atual
-    let date = new Date();
-    // calculo para o valor do ângulo
-    let angle = ((11 * minute - 60 * hour)/2);
-    // Arredonda o Ângulo
-    angle = Math.round(Math.abs(angle));
-    // Calcula o menor ângulo
-    if (angle > 180){
-        angle = 360 - angle
-    };
-    // Os dados só serão gravados se a hora estiver entre 0 e 11
-    if(hour >= 0 & hour <= 11){
-        const response = await pool.query('INSERT INTO public.clock (hour, minute, angle, date) VALUES ($1, $2, $3, $4)', [hour, minute, angle, date]);
-        res.json({
-            message: 'Clock cadastrado com sucesso',
-            clock: {hour, minute, angle, date}
-        })
-    } else{
-        res.json('O valor de horas deve está entre 0 e 11')
-    };
-};
-
-//Adiciona dados via URL tendo como parâmetro Hora e Minuto 
-const postClockHM = async (req, res) => {
-    const hour = parseInt(req.params.hour)
-    const minute = parseInt(req.params.minute)
-    // retorna a data atual
-    let date = new Date();
-
-    // calculo para o valor do ângulo
-    let angle = ((11 * minute - 60 * hour)/2);
-    // Arredonda o valor do ângulo
-    angle = Math.round(Math.abs(angle));
-    // Calcula o menor ângulo
-    if (angle > 180){
-        angle = 360 - angle
-    };
-    // Validador de horas
-    if(hour >= 0 & hour <= 11){
-        if(minute >= 0 & minute <=59){
-            const response = await pool.query('INSERT INTO public.clock (hour, minute, angle, date) VALUES ($1, $2, $3, $4)', [hour, minute, angle, date]);
-        res.json({
-            message: 'Clock cadastrado com sucesso',
-            clock: {hour, minute, angle, date}
-        })
-        } else{
-            res.json('O número de minutos deve estar entre 0 e 59')
+    
+    if(typeof response.rows[0] == "undefined"){
+        let date = new Date();
+        let angle = ((11 * minute - 60 * hour)/2);
+        angle = Math.round(Math.abs(angle));
+        if (angle > 180){
+            angle = 360 - angle
         };
-    } else{
-        res.json('O valor de horas deve está entre 0 e 11')
-    };
-};
-
-const putClock = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const { hour, minute } = req.body;
-
-    const response =await pool.query('UPDATE clock SET hour = $1, minute = $2 WHERE id = $3', [
-        hour,
-        minute,
-        id
-    ]);
-    res.json({
-        message: 'Clock Atualizado com Sucesso',
-        clock: {id, hour, minute}
-    });
+        if(hour >= 0 & hour <= 11){
+            if(minute >= 0 & minute <=59){
+                const insert = await pool.query('INSERT INTO public.clock (hour, minute, angle, date) VALUES ($1, $2, $3, $4)', [hour, minute, angle, date]);
+            res.json({
+                message: 'Clock cadastrado com sucesso',
+                clock: {hour, minute, angle, date}
+            })
+            } else{
+                res.json('O número de minutos deve estar entre 0 e 59')
+            };
+        } else{
+            res.json('O valor de horas deve está entre 0 e 11')
+        };
+        }else{
+            res.json(response.rows[0]);
+        }
 };
 
 const deleteClock = async (req, res) => {
@@ -118,8 +85,5 @@ module.exports = {
     getClock,
     getClockByH,
     getClockByHM,
-    postClockHour,
-    postClockHM,
-    putClock,
     deleteClock
 };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,14 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getClock, getClockByH, getClockByHM, postClockHour, postClockHM, putClock, deleteClock } = require('../controllers/index.controller');
+const { getClock, getClockByH, getClockByHM, postClockHour, postClockHM, deleteClock } = require('../controllers/index.controller');
 
-router.get('/clock', getClock); //retorna todos os valores
-router.get('/clock/:hour/', getClockByH); 
-router.get('/clock/:hour/:minute/', getClockByHM);
-router.post('/clock/:hour', postClockHour);
-router.post('/clock/:hour/:minute/', postClockHM);
-router.put('/clock/:id', putClock)
-router.delete('/clock/:id', deleteClock);
+router.get('/clock', getClock); //Retorna todos os clocks
+router.get('/clock/:hour/', getClockByH);//Retorna o ângulo baseado na hora e se não existir ele criará
+router.get('/clock/:hour/:minute/', getClockByHM);//Retorna o ângulo baseado na hora e minuto e caso não exista ele criará
+router.delete('/clock/:id', deleteClock);//Deletará o clock baseado no ID
 
 module.exports = router;
